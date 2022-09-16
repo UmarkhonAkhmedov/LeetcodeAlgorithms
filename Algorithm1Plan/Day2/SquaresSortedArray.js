c08163a7e4f5d709e29b2dba3241b4806e83ae86
umarkhon <umarkhonakhmedov2000@gmail.com>
2022-09-16 19:33:51 +0500

    Fixing some codes

--- Algorithm1Plan/Day2/SquaresSortedArray.js
+++ Algorithm1Plan/Day2/SquaresSortedArray.js
@@ -2,11 +2,7 @@ var sortedSquares = function (nums) {
   let newArray = [];
 
   for (let i = 0; i < nums.length; i++) {
-    if (nums[i] > 0) {
-      newArray.push(nums[i] ** 2);
-    } else {
-      newArray.push(nums[i] ** 2);
-    }
+    newArray.push(nums[i] ** 2);
   }
   const sorted = newArray.slice().sort((a, b) => a - b);
   return sorted;
